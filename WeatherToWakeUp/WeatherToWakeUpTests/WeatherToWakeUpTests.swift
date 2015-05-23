668214675e718ee0e9d6a760013dab143ccc5b50
Carola Nitz <nitz.carola@googlemail.com>
2015-05-23 15:33:55 +0200

    initial commit

---
+++ WeatherToWakeUp/WeatherToWakeUpTests/WeatherToWakeUpTests.swift
@@ -0,0 +1,36 @@
+//
+//  WeatherToWakeUpTests.swift
+//  WeatherToWakeUpTests
+//
+//  Created by Carola Nitz on 23/05/15.
+//  Copyright (c) 2015 weatherToWakeUp. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class WeatherToWakeUpTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
